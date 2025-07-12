cb14613c64d11e67b68e71c5488ecf1281202807
shhahad20 <Shhahad20@gmail.com>
2025-07-12 13:32:17 +0300

    🛠 fix: refactor express import and restore root route handler

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,7 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-// import express, { Request, Response } from 'express';
 import authRoutes from '../src/routes/authRoutes.js';
 import promptsRoutes from '../src/routes/promptsRoutes.js';
 import { aiChat } from "../src/controllers/aiController.js"; // wherever you put it
@@ -15,9 +14,11 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/pdf', promptsRoutes);
 app.post("/api/ai/chat", aiChat);
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!');
-// });
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
